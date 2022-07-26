8a314b00e9bf703507584417e72410fb3aa831ff
AnishBade <badeanish6045@gmail.com>
2022-07-26 23:33:15 +0545

    Allowed host to heroku

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-n_z-j9km-9p8#p@@)w!jxejhx77x%gwmd4f4&)_zbj*$#$$@%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'anishbade-weather-app.herokuapp.com'
+]
 
 
 # Application definition
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
